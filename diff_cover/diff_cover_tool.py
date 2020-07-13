4ff24f93f94bac2738aa5ba3dc2e0fba5369727e
zhangbiao11 <zhangbiao11@meituan.com>
2020-07-13 11:20:48 +0800

    add total number of coverage

--- diff_cover/diff_cover_tool.py
+++ diff_cover/diff_cover_tool.py
@@ -18,14 +18,14 @@ from diff_cover import DESCRIPTION, VERSION
 from diff_cover.diff_reporter import GitDiffReporter
 from diff_cover.git_diff import GitDiffTool
 from diff_cover.git_path import GitPathTool
-from diff_cover.report_generator import HtmlReportGenerator, StringReportGenerator, JsonReportGenerator
+from diff_cover.report_generator import HtmlReportGenerator, StringReportGenerator
 from diff_cover.violationsreporters.violations_reporter import XmlCoverageReporter
 
 HTML_REPORT_HELP = "Diff coverage HTML output"
-JSON_REPORT_HELP = "Diff coverage JSON output"
 COMPARE_BRANCH_HELP = "Branch to compare"
 CSS_FILE_HELP = "Write CSS into an external file"
 FAIL_UNDER_HELP = "Returns an error code if coverage or quality score is below this value"
+MIN_DIFF_LINE_HELP = "Ignore --fail-under if the changed lines in diff are less than this value"
 IGNORE_STAGED_HELP = "Ignores staged changes"
 IGNORE_UNSTAGED_HELP = "Ignores unstaged changes"
 EXCLUDE_HELP = "Exclude files, more patterns supported"
@@ -44,11 +44,10 @@ def parse_coverage_args(argv):
         {
             'coverage_xml': COVERAGE_XML,
             'html_report': None | HTML_REPORT,
-            'json_report': None | JSON_REPORT,
             'external_css_file': None | CSS_FILE,
         }
 
-    where `COVERAGE_XML`, `HTML_REPORT`, `JSON_REPORT`, and `CSS_FILE` are paths.
+    where `COVERAGE_XML`, `HTML_REPORT`, and `CSS_FILE` are paths.
 
     The path strings may or may not exist.
     """
@@ -61,22 +60,12 @@ def parse_coverage_args(argv):
         nargs='+'
     )
 
-    output_format = parser.add_mutually_exclusive_group()
-
-    output_format.add_argument(
+    parser.add_argument(
         '--html-report',
         metavar='FILENAME',
         type=str,
         default=None,
         help=HTML_REPORT_HELP
-    )
-
-    output_format.add_argument(
-        '--json-report',
-        metavar='FILENAME',
-        type=str,
-        default=None,
-        help=JSON_REPORT_HELP
     )
 
     parser.add_argument(
@@ -101,6 +90,14 @@ def parse_coverage_args(argv):
         type=float,
         default='0',
         help=FAIL_UNDER_HELP
+    )
+
+    parser.add_argument(
+        '--min-diff-line',
+        metavar='SCORE',
+        type=int,
+        default='0',
+        help=MIN_DIFF_LINE_HELP
     )
 
     parser.add_argument(
@@ -154,7 +151,6 @@ def parse_coverage_args(argv):
 
 def generate_coverage_report(coverage_xml, compare_branch,
                              html_report=None, css_file=None,
-                             json_report=None,
                              ignore_staged=False, ignore_unstaged=False,
                              exclude=None, src_roots=None, diff_range_notation=None):
     """
@@ -180,17 +176,12 @@ def generate_coverage_report(coverage_xml, compare_branch,
             with open(css_file, "wb") as output_file:
                 reporter.generate_css(output_file)
 
-    elif json_report is not None:
-        reporter = JsonReportGenerator(coverage, diff)
-        with open(json_report, "wb") as output_file:
-            reporter.generate_report(output_file)
-
     reporter = StringReportGenerator(coverage, diff)
     output_file = sys.stdout if six.PY2 else sys.stdout.buffer
 
     # Generate the report
     reporter.generate_report(output_file)
-    return reporter.total_percent_covered()
+    return reporter.total_num_lines(), reporter.total_percent_covered()
 
 
 def main(argv=None, directory=None):
@@ -207,11 +198,11 @@ def main(argv=None, directory=None):
     arg_dict = parse_coverage_args(argv[1:])
     GitPathTool.set_cwd(directory)
     fail_under = arg_dict.get('fail_under')
-    percent_covered = generate_coverage_report(
+    min_diff_line = arg_dict.get('min_diff_line')
+    total_diff_line, percent_covered = generate_coverage_report(
         arg_dict['coverage_xml'],
         arg_dict['compare_branch'],
         html_report=arg_dict['html_report'],
-        json_report=arg_dict['json_report'],
         css_file=arg_dict['external_css_file'],
         ignore_staged=arg_dict['ignore_staged'],
         ignore_unstaged=arg_dict['ignore_unstaged'],
@@ -220,7 +211,7 @@ def main(argv=None, directory=None):
         diff_range_notation=arg_dict['diff_range_notation']
     )
 
-    if percent_covered >= fail_under:
+    if total_diff_line <= min_diff_line or percent_covered >= fail_under:
         return 0
     else:
         LOGGER.error("Failure. Coverage is below {}%.".format(fail_under))
